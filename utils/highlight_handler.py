72dd299aa70bc58c75d86f2d23437aec2307b186
Hung Nguyen <hungnguyen.cosc@gmail.com>
2024-10-04 18:18:51 +0700

    #feat: improve processing highlight extractor

--- utils/highlight_handler.py
+++ utils/highlight_handler.py
@@ -9,31 +9,45 @@ def process_and_store_highlights():
     with open("highlights.txt", 'r', encoding='utf-8') as file:
         content = file.read()
 
-    # Adjusted regular expression to cover different cases
-    pattern = r"^(.*?)\n- Your (Highlight|Bookmark) on (page [0-9]+ \| )?Location ([0-9]+(?:-[0-9]+)?) \| Added on (.*?)\n\n(.*?)\n==========$"
-    matches = re.findall(pattern, content, re.MULTILINE | re.DOTALL)
+    # Split content by \n and remove empty lines
+    lines = content.split("\n")
+    lines = [line for line in lines if line.strip()]
+    # Determine the index of =======
+    indices = [i for i, line in enumerate(lines) if "==========" in line]
+
+    # Pre-proceed the highlights
+    pre_proceeded_highlights = []
+    for i in range(len(indices)):
+        if i == 0:
+            pre_proceed_highlight = lines[:indices[i]]
+        else:
+            pre_proceed_highlight = lines[indices[i - 1]+1:indices[i]]
+
+        # If the highlight is less than 3 lines, skip it because that highlight is not complete
+        if len(pre_proceed_highlight) < 3:
+            continue
+
+        pre_proceeded_highlights.append(pre_proceed_highlight)
 
     highlights = []
-    for match in matches:
-        book_title = match[0].strip()
-        item_type = match[1].strip()  # 'Highlight' or 'Bookmark'
-        # Handle missing page information
-        page_info = match[2].strip() if match[2] else "N/A"
-        location = match[3].strip()
-        timestamp = match[4].strip()
-        text = match[5].strip()
-
+    # Adjusted regular expression to cover different cases
+    pattern = r"^- Your (Highlight|Bookmark|Note) on (page [0-9]+ \| )?Location ([0-9]+(?:-[0-9]+)?) \| Added on (.*?)$"
+    for highlight in pre_proceeded_highlights:
+        match = re.findall(pattern, highlight[1], re.MULTILINE | re.DOTALL)
+        book_title = highlight[0].strip()
+        book_type = match[0][0].strip()
+        page_info = match[0][1].strip() if match[0][1] else "N/A"
+        location = match[0][2].strip()
+        timestamp = match[0][3].strip()
+        text = highlight[2].strip()
         highlights.append({
             "book_title": book_title,
-            "type": item_type,
-            "page_info": page_info,
+            # "type": book_type,
+            # "page_info": page_info,
             "location": location,
             "timestamp": timestamp,
             "text": text
         })
-
-    # Remove all highlights where text is empty
-    highlights = [highlight for highlight in highlights if highlight["text"]]
 
     # Save highlights to a JSON file
     with open("highlights.json", 'w', encoding='utf-8') as json_file:
@@ -53,3 +67,7 @@ def load_random_highlight():
     random_highlight = random.choice(highlights)
 
     return random_highlight
+
+
+if __name__ == "__main__":
+    process_and_store_highlights()
